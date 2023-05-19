57ab27c4352c669d1f3f4e403283007bdfb93b12
Jose Lanzos <jose.lanzos@example.com>
2023-05-19 09:33:19 +0200

    Add Changes

--- changelog_20230516-080855.sql
+++ changelog_20230516-080855.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
---changeset jose.lanzos:20230515-171435 labels: context:DEV
---comment: Add Table test_table_080
+--changeset jose.lanzos:20230516-080855 labels:sqlite002 context:DEV
+--comment: 
 CREATE TABLE test_table_201 (
 test_id INT,
 test_column VARCHAR(255),
---
+++ myfile.sql
@@ -0,0 +1,5 @@
+ CREATE TABLE test_table_080 (
+   test_id INT, 
+   test_column VARCHAR(255), 
+   PRIMARY KEY (test_id)
+) 
